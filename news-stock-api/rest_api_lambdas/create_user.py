73469c2ffb51951751eb9bd2993e317f2afb50f4
k7lim <k7lim@cs.stanford.edu>
2023-01-27 15:25:15 -0800

    starting balance code for new user, secret way people can set their own starting balance when creating the user hehehe

--- news-stock-api/rest_api_lambdas/create_user.py
+++ news-stock-api/rest_api_lambdas/create_user.py
@@ -21,16 +21,22 @@ def lambda_handler(event, context):
         if not all([user_id, first_name, last_name]):
             raise ValueError("Missing required field(s)")
 
+        # check if start_balance is present in the request
+        start_balance = 500 # default value
+        if 'start_balance' in user:
+            start_balance = user['start_balance']
+
         # insert the new user into the users table
         response = table.update_item(
             Key={
                 'user_id': user_id
             },
-            UpdateExpression='SET first_name = :fname, last_name = :lname',
+            UpdateExpression='SET first_name = :fname, last_name = :lname, balance = :balance',
             ConditionExpression='attribute_not_exists(user_id)',
             ExpressionAttributeValues={
                 ':fname': first_name,
-                ':lname': last_name
+                ':lname': last_name,
+                ':balance': start_balance
             },
             ReturnValues='ALL_NEW'
         )
@@ -41,7 +47,8 @@ def lambda_handler(event, context):
             'body': json.dumps({
                 'user_id': user_id,
                 'first_name': first_name,
-                'last_name': last_name
+                'last_name': last_name,
+                'balance': start_balance
             })
         }
     except ValueError as e:
